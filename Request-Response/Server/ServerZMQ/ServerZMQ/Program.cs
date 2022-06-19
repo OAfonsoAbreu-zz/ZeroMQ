f1c63e5876ced7e2ec4cf9ddfba5ebeb61657c4d
Afonso Abreu <afonsohenrique2@hotmail.com>
2022-06-19 11:48:41 -0300

    Criação do server ZeroMQ para Request-Response

---
+++ Request-Response/Server/ServerZMQ/ServerZMQ/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace ServerZMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length < 1)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Usage: ./{0} HWServer [ServerZMQ]", AppDomain.CurrentDomain.FriendlyName);
+                Console.WriteLine();
+                Console.WriteLine("    Name   Afonso. Default: World");
+                Console.WriteLine();
+                args = new string[] { "World" };
+            }
+
+            string name = args[0];
+
+            using (var server = new ResponseSocket())
+            {
+                server.Bind("tcp://*:5555");
+                while (true)
+                {
+                    var msg = server.ReceiveFrameString();
+
+                    if(msg.ToUpper() == "HELLO")
+                    {
+                        Console.WriteLine("Received {0}", msg);
+                        Thread.Sleep(1);
+                        server.SendFrame(name);
+                    }
+                    else
+                    {
+                        Console.WriteLine("The message was not recognized");
+                    }
+                        
+                }
+            }
+        }
+    }
+}
