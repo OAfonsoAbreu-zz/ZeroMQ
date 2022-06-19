7bfbf0bd2e0038b4dd0556eba009cdccc1b99e66
Afonso Abreu <afonsohenrique2@hotmail.com>
2022-06-19 14:33:28 -0300

    Ciação de Publish-Subscribe

---
+++ Publish-Subscribe/Publisher/PublisherZMQ/PublisherZMQ/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace PublisherZMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string address = "tcp://*:5555";
+            Console.WriteLine("I: Publisher.Bind'ing on {0}", address);
+
+            using (var publisher = new PublisherSocket())
+            {
+                publisher.Bind(address);
+
+                var rnd = new Random();
+
+                while (true)
+                {
+                    // Pega CEP e Temperatura do aleatorio
+                    int zipcode = rnd.Next(99999);
+                    int temperature = rnd.Next(-55, +45);
+
+                    var update = string.Format("{0:D5} {1}", zipcode, temperature); //topic msg
+
+                    var topic = string.Format("{0:D5}", zipcode);
+                    var msg = string.Format("{0}", temperature); 
+
+                    Console.WriteLine("Sending --> CEP: {0} | Temperatura: {1}°C", topic, msg);
+
+                    //Envia para todos subscribers
+                    publisher
+                        .SendFrame(update);
+                        //.SendMoreFrame(topic)
+                        //.SendFrame(msg);
+
+
+                }
+            }
+        }
+    }
+}
---
+++ Publish-Subscribe/Subscriber/SubscriberZMQ/SubscriberZMQ/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace SubscriberZMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			if (args == null || args.Length < 2)
+			{
+				Console.WriteLine();
+				Console.WriteLine("Usage: ./{0} WUClient [ZipCode] [Endpoint]", AppDomain.CurrentDomain.FriendlyName);
+				Console.WriteLine();
+				Console.WriteLine("    ZipCode   The zip code to subscribe. Default is 72622 Nürtingen");
+				Console.WriteLine("    Endpoint  Where WUClient should connect to.");
+				Console.WriteLine("              Default is tcp://127.0.0.1:5556");
+				Console.WriteLine();
+				if (args.Length < 1)
+					args = new string[] { "72622", "tcp://127.0.0.1:5555" };
+				else
+					args = new string[] { args[0], "tcp://127.0.0.1:5555" };
+			}
+
+			string endpoint = args[1];
+
+			using (var subscriber = new SubscriberSocket())
+            {
+				Console.WriteLine("I: Connecting to {0}...", endpoint);
+				subscriber.Connect(endpoint);
+
+				// Subscribe to zipcode
+				string zipCode = args[0];
+				Console.WriteLine("I: Subscribing to zip code {0}...", zipCode);
+				subscriber.Subscribe(zipCode);
+
+				// Process 10 updates
+				int i = 0;
+				long total_temperature = 0;
+				for (; i < 20; ++i)
+				{
+
+					string reply = subscriber.ReceiveFrameString();
+
+					Console.WriteLine(reply);
+					total_temperature += Convert.ToInt64(reply.Split(' ')[1]);
+					
+				}
+				Console.WriteLine("Average temperature for zipcode '{0}' was {1}°C", zipCode, (total_temperature / i));
+
+			}
+        }
+    }
+}
